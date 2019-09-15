32b548d0371d8b8eb67d4662e35ed8713aa005ce
Aizadik <aizadaboposhova@gmail.com>
2019-09-15 12:45:24 -0500

    setted up code to connect mysgl and node

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var mysql = require("mysql");
+
+var connection;
+
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection.connect();
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "utbootcamp",
+        database: "burgers_db"
+    });
+};
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
