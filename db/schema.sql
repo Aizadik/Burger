f55c52f1c9720d3b412a7ccf1673f25e25912a53
Aizadik <aizadaboposhova@gmail.com>
2019-09-21 16:20:16 -0500

    adding chnages

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
+CREATE DATABASE burgers_db;
 USE burgers_db;
 
 DROP TABLE IF EXISTS burgers;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
 var PORT = process.env.PORT || 3000;
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
@@ -9,19 +8,19 @@ app.use(express.static(__dirname + "/public"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgersController.js");
+// var routes = require("./controllers/burgersController.js");
 
-app.use("/", routes);
-app.use("/update", routes);
-app.use("/create", routes);
+// app.use("/", routes);
+// app.use("/update", routes);
+// app.use("/create", routes);
 
 
 app.listen(PORT, function() {
-    console.log("24k magic happens on port: " + PORT);
+    console.log("localhost listening on: " + PORT);
 });
